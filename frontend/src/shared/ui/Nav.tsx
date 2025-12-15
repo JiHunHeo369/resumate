06a0920c2e98b35bdb6682e7d8edf5c8dc97bf73
jezeony <jezeony@gmail.com>
2025-12-15 18:41:56 +0900

    feat : Nav 라우터 이동 추가

--- frontend/src/shared/ui/Nav.tsx
+++ frontend/src/shared/ui/Nav.tsx
@@ -1,16 +1,29 @@
 'use client';
-
+import { redirect } from 'next/navigation';
 import { useState } from 'react';
 import Button from './Button';
 
 export default function Nav() {
-  const [activeMenu, setActiveMenu] = useState(0);
+  const [activeMenu, setActiveMenu] = useState<number>(-1);
+
+  const handleMenuClick = ({ url, idx }: { url: string; idx?: number }) => {
+    setActiveMenu(idx!);
+
+    redirect(`/${url}`);
+  };
 
   return (
     <nav className='flex items-center justify-between px-28 py-3 h-16 border-b  border-slate-200 sticky top-0 bg-white'>
       <div className='flex items-center gap-8 '>
         <div className='flex text-lg font-bold'>
-          <h2>RESUMATE</h2>
+          <h2>
+            <button
+              className='cursor-pointer'
+              onClick={() => handleMenuClick({ url: 'home', idx: -1 })}
+            >
+              RESUMATE
+            </button>
+          </h2>
         </div>
 
         <ul className='flex items-center text-base font-semibold text-black gap-1 list-none p-0 m-0'>
@@ -23,17 +36,28 @@ export default function Nav() {
                   : 'px-2 py-5'
               }
             >
-              <button className='appearance-none bg-none border-0 p-0 m-0 focus:outline-none'>
-                {menu}
+              <button
+                className='appearance-none bg-none border-0 p-0 m-0 focus:outline-none cursor-pointer hover:text-[#88cc50]'
+                onClick={() => handleMenuClick({ url: menu.id, idx })}
+              >
+                {menu.name}
               </button>
             </li>
           ))}
         </ul>
       </div>
 
-      <Button onClick={() => console.log('로그인 클릭')}>로그인</Button>
+      <Button
+        className='cursor-pointer hover:opacity-90'
+        onClick={() => handleMenuClick({ url: 'login' })}
+      >
+        로그아웃
+      </Button>
     </nav>
   );
 }
 
-const MENU_MAP = ['HOME', '이력서 등록', 'MY'];
+const MENU_MAP = [
+  { id: 'create', name: '이력서 등록' },
+  // { id: 'my', name: 'MY' },
+];
